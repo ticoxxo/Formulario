7eee3d74938e82389099585f2d6bc59d842897a3
Ticoxxo <ticoxxo@gmail.com>
2024-05-24 10:56:05 -0700

    Initial Commit

---
+++ Formulario/FormularioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormularioApp.swift
+//  Formulario
+//
+//  Created by Alberto Almeida on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FormularioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
